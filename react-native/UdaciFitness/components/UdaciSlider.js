e67cceb318523242848a37ab345881e73b4ed2ae
Diogo Bernardelli <diogobernardelli@jera.com.br>
2019-01-21 20:35:06 -0300

    Feature: Styling all react native components using StyleSheet library, icons, conditionals to apply singular identity to each platform and fixing some minor issues

--- react-native/UdaciFitness/components/UdaciSlider.js
+++ react-native/UdaciFitness/components/UdaciSlider.js
@@ -1,21 +1,35 @@
 import React from 'react'
-import { View, Text, Slider } from 'react-native'
+import { View, Text, StyleSheet, Slider } from 'react-native'
+import { gray } from '../utils/colors'
 
 export default function UdaciSlider ({ max, unit, step, value, onChange }) {
-  // const { value, onChange, state } = this.props
   return (
-    <View>
+    <View style={styles.row}>
       <Slider
+        style={{flex: 1}}
         minimumValue={0}
         maximumValue={max}
         step={step}
         value={value}
         onValueChange={onChange}
       />
-      <View>
-        <Text>{value}</Text>
-        <Text>{unit}</Text>
+      <View style={styles.metricCounter}>
+        <Text style={{fontSize: 24, textAlign: 'center'}}>{value}</Text>
+        <Text style={{fontSize: 18, color: gray}}>{unit}</Text>
       </View>
     </View>
   )
 }
+
+const styles = StyleSheet.create({
+  row: {
+    flexDirection: 'row',
+    flex: 1,
+    alignItems: 'center',
+  },
+  metricCounter: {
+    width: 85,
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+})
